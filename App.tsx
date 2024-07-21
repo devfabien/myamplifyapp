f2e029106d17af4c6b14df2b8a32d89e30ea5027
devfabien <ish001fabi@gmail.com>
2024-07-21 14:53:13 +0200

    refactor: remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import RootNavigation from "./src/Navigation/rootnavigation";
 
 export default function App() {
